02f2c9746324bf9636374f0756b737f5eab2fba7
JohnFonsecs <jvfc2000@hotmail.com>
2024-08-12 20:27:19 -0300

    Add Cliente e Fabricante

--- Locacoes/Controllers/ClienteController.cs
+++ Locacoes/Controllers/ClienteController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Locacoes.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Locacoes.Controllers
 {
     public class ClienteController : Controller
     {
+        public static List<Cliente> Clientes = new List<Cliente>
+        {
+            new Cliente { Id = 1, Nome = "João", Email = "johnfon@gmail.com", 
+                Telefone = "(14)999999999", Cidade = "São Paulo" },
+
+            new Cliente { Id = 2, Nome = "Maria", Email = "maria@gmail.com", 
+                Telefone = "(21)888888888", Cidade = "Rio de Janeiro" },
+        };
         public IActionResult Index()
         {
-            return View();
+            return View(Clientes);
         }
     }
 }
---
+++ Locacoes/Controllers/FabricanteController.cs
@@ -0,0 +1,19 @@
+﻿using Locacoes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locacoes.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        public static List<Fabricante> Fabricantes = new List<Fabricante>
+        {
+            new Fabricante { Id = 1, Nome = "Fiat", Pais = "Itália" },
+            new Fabricante { Id = 2, Nome = "Volkswagen", Pais = "Alemanha" },
+            new Fabricante { Id = 3, Nome = "Ford", Pais = "Estados Unidos" },
+        };
+        public IActionResult Index()
+        {
+            return View(Fabricantes);
+        }
+    }
+}
