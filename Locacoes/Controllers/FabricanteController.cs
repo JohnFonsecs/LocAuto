46505f73de035613fe04b03dc55cb67f0dcbd489
JohnFonsecs <jvfc2000@hotmail.com>
2024-09-23 20:45:48 -0300

    Adição de CRUD para Modelos e melhorias gerais
    - Adicionados namespaces necessários e removidos desnecessários em `ClienteController.cs` e `FabricanteController.cs`.
    - Implementados métodos CRUD em `ClienteController` e `FabricanteController`.
    - Adicionado `ModeloController` com métodos CRUD.
    - Modificado `LocacoesContext.cs` para incluir `Fabricante` e `Modelo`.
    - Atualizado `Locacoes.csproj` para incluir a pasta `Migrations`.
    - Removido código comentado e desatualizado nos controladores.
    - Atualizadas views `Create.cshtml`, `Delete.cshtml`, `Details.cshtml`, `Edit.cshtml` e `Index.cshtml`.
    - Adicionada view `Index.cshtml` para `Modelo`.
    - Atualizado `_Layout.cshtml` para incluir link de navegação para `Modelos`.
    - Removidos arquivos de migração antigos e adicionadas novas migrações.
    - Corrigidos erros de formatação e melhorada a legibilidade do código.
    - Re-Adicionada migração `InitialCreate` para criar tabelas `Clientes`, `Fabricantes` e `Modelos`.
    - Adicionada classe `Modelo` com propriedades `Id`, `Nome`, `Ano`, `Fabricante` e `FabricanteId`.
    - Reformulada views `Create.cshtml`, `Delete.cshtml`, `Details.cshtml`, `Edit.cshtml` e `Index.cshtml` para `Fabricante` e `Modelo`.

--- Locacoes/Controllers/FabricanteController.cs
+++ Locacoes/Controllers/FabricanteController.cs
@@ -1,31 +1,157 @@
-﻿using Locacoes.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Locacoes.Data;
+using Locacoes.Models;
 
 namespace Locacoes.Controllers
 {
     public class FabricanteController : Controller
     {
-        public static List<Fabricante> Fabricantes = new List<Fabricante>
+        private readonly LocacoesContext _context;
+
+        public FabricanteController(LocacoesContext context)
         {
-            new Fabricante { Id = 1, Nome = "Fiat", Pais = "Itália" },
-            new Fabricante { Id = 2, Nome = "Volkswagen", Pais = "Alemanha" },
-            new Fabricante { Id = 3, Nome = "Ford", Pais = "Estados Unidos" },
-        };
-        public IActionResult Index()
-        {
-            return View(Fabricantes);
+            _context = context;
         }
+
+        // GET: Fabricante
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Fabricantes.ToListAsync());
+        }
+
+        // GET: Fabricante/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fabricante = await _context.Fabricantes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fabricante == null)
+            {
+                return NotFound();
+            }
+
+            return View(fabricante);
+        }
+
+        // GET: Fabricante/Create
         public IActionResult Create()
         {
             return View();
         }
+
+        // POST: Fabricante/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Fabricante fabricante)
+        public async Task<IActionResult> Create([Bind("Id,Nome,Pais")] Fabricante fabricante)
         {
-            fabricante.Id = Fabricantes.Select(x => x.Id).Max() +1;
-            Fabricantes.Add(fabricante);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _context.Add(fabricante);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fabricante);
+        }
+
+        // GET: Fabricante/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fabricante = await _context.Fabricantes.FindAsync(id);
+            if (fabricante == null)
+            {
+                return NotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: Fabricante/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Pais")] Fabricante fabricante)
+        {
+            if (id != fabricante.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fabricante);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FabricanteExists(fabricante.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fabricante);
+        }
+
+        // GET: Fabricante/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fabricante = await _context.Fabricantes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fabricante == null)
+            {
+                return NotFound();
+            }
+
+            return View(fabricante);
+        }
+
+        // POST: Fabricante/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fabricante = await _context.Fabricantes.FindAsync(id);
+            if (fabricante != null)
+            {
+                _context.Fabricantes.Remove(fabricante);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FabricanteExists(int id)
+        {
+            return _context.Fabricantes.Any(e => e.Id == id);
         }
     }
 }
--- Locacoes/Migrations/20240923230441_InitialCreate.Designer.cs
+++ Locacoes/Migrations/20240923230441_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using Locacoes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Locacoes.Migrations
 {
     [DbContext(typeof(LocacoesContext))]
-    partial class LocacoesContextModelSnapshot : ModelSnapshot
+    [Migration("20240923230441_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
